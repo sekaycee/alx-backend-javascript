44c44dfeb70af08575cebc9ff0d265c4e942d609
Kay Cee <se.kaycee@gmail.com>
2023-07-06 02:25:35 +0100

    feat: build table of students in task_0

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -19,5 +19,25 @@ const secondStudent: Student = {
 	location: "London, UK",
 };
 
-const studentList: Student[] = [firstStudent, secondStudent];
+const studentsList: Student[] = [firstStudent, secondStudent];
 
+// Render a table and append a new row for each student in the array
+const table = document.createElement("table");
+const tbody = document.createElement("tbody");
+
+studentsList.forEach((student) => {
+  const row = document.createElement("tr");
+
+  const firstNameCell = document.createElement("td");
+  firstNameCell.textContent = student.firstName;
+  row.appendChild(firstNameCell);
+
+  const locationCell = document.createElement("td");
+  locationCell.textContent = student.location;
+  row.appendChild(locationCell);
+
+  tbody.appendChild(row);
+});
+
+table.appendChild(tbody);
+document.body.appendChild(table);
